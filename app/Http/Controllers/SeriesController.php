56ec2fe0f4693b9100b9788fe552538a1f267481
Marcus Neri <marcusneri_joga10@hotmail.com>
2020-09-04 11:35:55 -0300

    Arquivos projeto

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Serie;
+
+
+class SeriesController extends BaseController
+{
+    public function __construct()
+    {
+        $this->classe = Serie::class;
+    }
+}
